afa99e5651f8eda01c6d44671db730516b9a913c
Kenan <kananmh@code.edu.az>
2023-02-01 02:42:40 +0400

    task

---
+++ task1/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        //Verilmis yazinin evvelindeki bosluqlari silib qaytaran metod
+        {
+            Console.WriteLine(deletethespace("   salam necesen   "));
+        }
+        static string deletethespace(string name)
+        {
+            string word ="";
+            for(int i=0;i<name.Length;i++)
+            {
+                if (name[i] != ' ')
+                    
+                    word += name[i];
+            }
+            return word;
+        }
+    }
+}
---
+++ task2/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        // Verilmis ededler siyahisindan yalniz musbet ededler qalan yeni bir siyahi duzelden metod
+        {
+            int[] num = { -56, -666, -2324, 23443, -2243, 432 };
+            int[] newnum = positive(num);
+            for(int i=0;i<num.Length;i++)
+            {
+                Console.WriteLine(newnum[i]);
+            }
+        }
+        static int[] positive(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            for(int i=0;i<nums.Length;i++)
+            {
+                if (nums[i] > 0)
+                result[i] = nums[i];
+            }
+            return result;
+        }
+        
+        
+    }
+}
